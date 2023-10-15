ebc3e295df78a5fb4f98e77bcde23fac5074e047
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2023-10-15 11:57:05 +0200

    Add files via upload

---
+++ 09_BiggerProgram/main.cpp
@@ -0,0 +1,22 @@
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+import spreadsheet;
+
+Spreadsheet createObject()
+{
+	return Spreadsheet{ 3, 2 };
+}
+
+int main()
+{
+	std::vector<Spreadsheet> vec;
+	for (size_t i{ 0 }; i < 2; ++i) {
+		std::cout << "Iteration " << i << std::endl;
+		vec.push_back(Spreadsheet{ 100, 100 });
+		std::cout << std::endl;
+	}
+
+	Spreadsheet s{ 2, 3 };
+}
